f148b6de77ed961d57339de64882ac62d3dddae6
Nabeel Syed <nabeelsyed88@yahoo.com>
2018-01-07 16:50:41 -0500

    Added Utility Class for Instragram Sharing

---
+++ app/src/main/java/com/example/chris/memegenerator/util/InstagramHandler.java
@@ -0,0 +1,36 @@
+package com.example.chris.memegenerator.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import java.io.File;
+
+import static android.support.v4.content.ContextCompat.startActivity;
+
+/**
+ * Created by Nabeel on 1/7/2018.
+ */
+
+public class InstagramHandler {
+
+    public static void createInstagramIntent(String type, String mediaPath, Context context) {
+
+        // Create the new Intent using the 'Send' action.
+        Intent share = new Intent(Intent.ACTION_SEND);
+
+        // Set the MIME type
+        share.setType(type);
+
+        // Create the URI from the media
+        File media = new File(mediaPath);
+        Uri uri = Uri.fromFile(media);
+
+        // Add the URI to the Intent.
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+
+        // Broadcast the Intent.
+        startActivity(context,share,null);
+    }
+
+}
